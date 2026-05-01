e15866b2c8011338dfa6ff3c5992c0c685296c24
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-01 15:14:43 +0700

    menambahkan step 1step 2 dan iff

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -28,3 +28,12 @@ public:
         int nim;
         cout << "Enter the roll number of the student: ";
         cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newnode = new Node();
+
+        // Step 2: Assign value to the data field
+        newnode->no = nim;
+
+        // If list is empty or inserting at 1st is empty or not in sorted
+        if (START == NULL || nim <= START->no)
